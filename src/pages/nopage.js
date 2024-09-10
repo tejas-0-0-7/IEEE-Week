7ff29b020394d1ad62ad0708c61c0391e9e62581
Tejas <tejadprtp@gmail.com>
2024-09-10 15:11:18 +0530

    Error page and some mod in nav

--- src/pages/nopage.js
+++ src/pages/nopage.js
@@ -1,12 +1,23 @@
+import React from 'react';
 import Navbaring from '../Components/Nav';
 import Footer from '../Components/Foot';
+import './ErrorPage.css'; // Import the CSS file for styling
+
 const Nopage = () => {
     return ( 
         <div>
             <Navbaring />
+            <div className="error-page">
+                <div className="error-content">
+                    <h1>404</h1>
+                    <h2>Page Not Found</h2>
+                    <p>Sorry, the page you are looking for does not exist.</p>
+                    <a href="/">Go back to Home</a>
+                </div>
+            </div>
             <Footer />
         </div>
-     );
+    );
 }
- 
+
 export default Nopage;
