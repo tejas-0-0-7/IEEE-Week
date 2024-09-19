ca7d5211c4307ab05a799ccec965d501f6c3bda2
Tejas <tejadprtp@gmail.com>
2024-09-20 03:55:55 +0530

    Done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,9 +11,14 @@ function Home() {
   return (
     <div>
         <Navbaring />
+        <div className="H-container">
+          <h1><center>BMSCE IEEE Presents</center></h1>
+          <h1><center>IEEE Week 2024</center></h1>
+          <h1><center>October 21<sup>st</sup  ></center></h1>
+        </div>
         <CountDown />
         <div className='center'>
-        <Slider  data={data} activeSlide={5} />
+          <Slider  data={data} activeSlide={5} />
         </div>
         <Footer />
     </div>
