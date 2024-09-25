882073790ba7934fcee2e3b6ed93fb0a805ba094
Tejas <tejadprtp@gmail.com>
2024-09-25 21:54:55 +0530

    Few changes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Events from './pages/Events';
 import Contact from './pages/contact';
 import Meet from './pages/Meet';
 import Membership from './pages/Membership';
+import Membershipone from './pages/Membershipact';
 import Nopage from './pages/nopage';
 import EventDescription from './pages/EventDescription';
 import './App.css';
@@ -18,6 +19,7 @@ function App() {
           <Route path="/events" element={<Events />} />
           <Route path="/meet" element={<Meet />} />
           <Route path="/membership" element={<Membership />} />
+          <Route path="/membershipact" element={<Membershipone />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/eventdescription" element={<EventDescription />} />
           <Route path="*" element={<Nopage />} />
--- src/pages/Membershipact.js
+++ src/pages/Membershipact.js
@@ -1,16 +1,15 @@
 import Navbaring from '../Components/Nav';
 import Footer from '../Components/Foot';
-import CountDown from '../Components/CountDownMem';
+// import CountDown from '../Components/CountDownMem';
 // import MdForm from '../Components/Form';
-// import './Membership.css';
-const Membership = () => {
+import './Membership.css';
+const Membershipone = () => {
     return ( 
         <div>
             <Navbaring />
-            <CountDown/>
             <Footer />
         </div>
      );
 }
  
-export default Membership;
+export default Membershipone;
