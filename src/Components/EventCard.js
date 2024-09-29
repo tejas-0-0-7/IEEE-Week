d6c0eef9dda351f66a2954824940304a47a02ef6
dishal35 <ldisha1103@gmail.com>
2024-09-29 16:36:58 +0530

    Minor changes

--- src/Components/EventCard.js
+++ src/Components/EventCard.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './EventCard.css';
 import { useNavigate } from 'react-router-dom'; 
-import EventDescription from '../pages/EventDescription';
 
 function EventCard(props) {
+
   const navigate = useNavigate(); 
   const handleClick = () => {
     navigate('/eventdescription'); 
