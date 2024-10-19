86f9dd8dbb03dbd833003a91e9c735eb91ea4409
Tejas <tejadprtp@gmail.com>
2024-10-19 22:38:41 +0530

    Connection

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,33 @@
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const connectDB = require('./config/db');
-const eventsRouter = require('./routes/events');
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const eventsRouter = require("./routes/events");
 
 dotenv.config();
 
 const app = express();
 
 // Connect to MongoDB
-console.log('Attempting to connect to MongoDB...');
+console.log("Attempting to connect to MongoDB...");
 connectDB()
-  .then(() => console.log('MongoDB connected successfully'))
-  .catch(err => console.error('MongoDB connection error:', err));
+  .then(() => console.log("MongoDB connected successfully"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
-// Routes
-app.use('/api/events', eventsRouter);
-
-app.get('/', (req, res) => {
-  res.send('Hello from Express!');
+//Tejas trial start
+app.use(express.static(path.join(__dirname, "build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
+//Tejas trial end
+
+// Routes
+app.use("/api/events", eventsRouter);
 
 const PORT = process.env.PORT || 5000;
 
