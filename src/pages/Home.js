d7d9604e19f31c1c296a079106eb32f1648835fc
Vicky <vigneshbhaskar54@gmail.com>
2024-10-26 12:04:26 +0530

    rhh

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,33 +5,14 @@ import Slider from '../Components/Slider.js';
 import './Slider.css';
 import fetchEvents from '../Components/data.js';
 import CountDown from '../Components/CountDown.js';
+import AboutUs from '../Components/about_us.js';
 import Overtheyears from '../Components/Overtheyears'; // Import the new component
 
-
 function Home() {
-  // const initialVideoRef = useRef(null);
-  // const backgroundVideoRef = useRef(null);
   const vantaRef = useRef(null);
   const [events, setEvents] = useState([]);
 
   useEffect(() => {
- //   const hasWatchedVideo = localStorage.getItem('hasWatchedVideo');
-
-    // if (!hasWatchedVideo) {
-    //   initialVideoRef.current.style.display = 'block';
-    //   initialVideoRef.current.play();
-
-    //   initialVideoRef.current.addEventListener('ended', () => {
-    //     initialVideoRef.current.style.display = 'none';
-    //     backgroundVideoRef.current.style.display = 'block';
-    //     backgroundVideoRef.current.play();
-    //     localStorage.setItem('hasWatchedVideo', 'true');
-    //   });
-    // } else {
-    //   initialVideoRef.current.style.display = 'none';
-    //   backgroundVideoRef.current.style.display = 'block';
-    //   backgroundVideoRef.current.play();
-    // }
     const vantaEffect = window.VANTA.NET({
       el: ".net",
       mouseControls: true,
@@ -41,14 +22,14 @@ function Home() {
       minWidth: 200.00,
       scale: 1.00,
       scaleMobile: 1.00,
-      color: 0xdaa520,
+      color: 0x428679,
       backgroundColor: 0x0,
-      points: 13.00,
+      points: 25.00,
       maxDistance: 16.00,
-      spacing: 20.00,
+      spacing: 15.00,
       showDots: false
-    })
-    
+    });
+
     const loadEvents = async () => {
       const fetchedEvents = await fetchEvents();
       const eventImages = fetchedEvents.map(event => ({ id: event._id, image: event.image }));
@@ -58,20 +39,8 @@ function Home() {
   }, []);
 
   return (
-    
     <div>
       <Navbaring />
-
-      {/* <div className="video-container">
-        <video ref={initialVideoRef} id="video-initial" autoPlay muted>
-          <source src="intro.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-        <video ref={backgroundVideoRef} id="video-background" autoPlay muted>
-          <source src="static.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-      </div> */}
 
       <div className="countdown-container">
         <CountDown />
@@ -102,6 +71,9 @@ function Home() {
             <button className="know-more-button">Know More</button>
           </div>
           <img src="/images/sod.jpeg" alt="Flagship Event Poster" className="poster" />
+        </div>
+        <div>
+        <AboutUs />
         </div>
       </div>
     </div>
