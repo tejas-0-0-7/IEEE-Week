36ec596bda17cab3111ac174c66c941289848cb7
Tejas <tejadprtp@gmail.com>
2024-10-30 14:47:34 +0530

    Deploy

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Events from './pages/Events';
+import Contact from './pages/contact';
+import Meet from './pages/Meet';
+import Membership from './pages/Membership';
+import Membershipone from './pages/Membershipact';
+import Nopage from './pages/nopage';
+// import Loader from './Components/loader'; 
+import './App.css';
+
+
+function App() {
+  // const [loading, setLoading] = useState(true);
+
+  // useEffect(() => {
+  //   const timer = setTimeout(() => {
+  //     setLoading(false);
+  //   }, 2000); // Simulate a loading delay
+
+  //   return () => clearTimeout(timer);
+  // }, []);
+
+  // if (loading) {
+  //   return <Loader />;
+  // }
+
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />}/>
+          <Route path="/home" element={<Home />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/meet" element={<Meet />} />
+          <Route path="/membership" element={<Membership />} />
+          <Route path="/membershipact" element={<Membershipone />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<Nopage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
