919c957b4976ac5d7a95addfc2f098bbd0ea5c59
Tejas <tejadprtp@gmail.com>
2024-10-30 14:36:35 +0530

    deployyyyyyy

--- src/Components/CountDown.js
+++ src/Components/CountDown.js
@@ -10,7 +10,7 @@ const CountDown = () => {
     let interval = useRef(null); // Correct initialization of interval
 
     const startTimer = () => {
-        const countDownDate = new Date('Oct 21, 2024 00:00:00').getTime();
+        const countDownDate = new Date('Nov 11, 2024 00:00:00').getTime();
 
         interval.current = setInterval(() => {
             const now = new Date().getTime();
