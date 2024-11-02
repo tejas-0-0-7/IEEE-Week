b2b87d4df3edcb16031fb720d1afdc134a6a2dfc
Tejas <tejadprtp@gmail.com>
2024-11-02 12:06:18 +0530

    Update: Final changes

--- src/Components/Slider.js
+++ src/Components/Slider.js
@@ -1,115 +1,138 @@
-import React, { useState, useEffect } from 'react';
-import '../pages/Slider.css';
-
-const Slider = ({ data, activeSlide: initialActiveSlide = 0 }) => {
-  const [activeSlide, setActiveSlide] = useState(initialActiveSlide);
-  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
-  const [touchStartX, setTouchStartX] = useState(null); // Added state for touch start position
-
-  useEffect(() => {
-    const handleResize = () => setIsMobile(window.innerWidth <= 768);
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
-
-  useEffect(() => {
-    const handleTouchStart = (e) => {
-      setTouchStartX(e.touches[0].clientX);
-    };
-
-    const handleTouchMove = (e) => {
-      if (!touchStartX) return;
-      const touchEndX = e.touches[0].clientX;
-      const diffX = touchStartX - touchEndX;
-
-      if (diffX > 50) {
-        // Swipe left
-        next();
-      } else if (diffX < -50) {
-        // Swipe right
-        prev();
-      }
-
-      setTouchStartX(null);
-    };
-
-    if (isMobile) {
-      window.addEventListener('touchstart', handleTouchStart);
-      window.addEventListener('touchmove', handleTouchMove);
-    }
-    return () => {
-      if (isMobile) {
-        window.removeEventListener('touchstart', handleTouchStart);
-        window.removeEventListener('touchmove', handleTouchMove);
-      }
-    };
-  }, [activeSlide, isMobile, touchStartX]);
-
-  const next = () => activeSlide < data.length - 1 && setActiveSlide(activeSlide + 1);
-  const prev = () => activeSlide > 0 && setActiveSlide(activeSlide - 1);
-
-  const getStyles = (index) => {
-    const offsetMultiplier = isMobile ? 1.2 : 2;
-    const translateXBase = isMobile ? 150 : 240;
-    const translateZBase = isMobile ? -200 : -300;
-
-    if (activeSlide === index) {
-      return {
-        opacity: 1,
-        transform: `translateX(0px) translateZ(0px) rotateY(0deg)`,
-        zIndex: 10,
-      };
-    } else if (Math.abs(activeSlide - index) <= 3) {
-      const direction = index > activeSlide ? 1 : -1;
-      const offset = Math.abs(activeSlide - index);
-      return {
-        opacity: 1,
-        transform: `translateX(${direction * translateXBase * offset}px) 
-                    translateZ(${translateZBase - offset * 100}px) 
-                    rotateY(${direction * 30 * offsetMultiplier}deg)`,
-        zIndex: 10 - offset,
-      };
-    }
-    return { opacity: 0, zIndex: 1 };
-  };
-
-  if (!data || data.length === 0) {
+import React,{useState} from 'react'
+import '../pages/Slider.css'
+export default (props) => {
+  console.log(props);
+  const [activeSlide, setactiveSlide] = useState(props.activeSlide || 0);
+  if(!props.data || props.data.length === 0){
     return <div>Loading...</div>;
   }
+  const handleSlideClick = (index) => {
+    setactiveSlide(index); 
+  };
 
+  const next = () =>
+    activeSlide < props.data.length - 1 && setactiveSlide(activeSlide + 1);
+
+  const prev = () => activeSlide > 0 && setactiveSlide(activeSlide - 1);
+
+  const getStyles = (index) => {
+    if (activeSlide === index) {
+      // Center card (fully visible)
+      return {
+        opacity: 1,
+        transform: "translateX(0px) translateZ(0px) rotateY(0deg)",
+        zIndex: 10
+      };
+    } 
+    else if (activeSlide - 1 === index) {
+      // First card to the left
+      return {
+        opacity: 1,
+        transform: "translateX(-240px) translateZ(-300px) rotateY(30deg)",
+        zIndex: 9
+      };
+    } 
+    else if (activeSlide + 1 === index) {
+      // First card to the right
+      return {
+        opacity: 1,
+        transform: "translateX(240px) translateZ(-300px) rotateY(-30deg)",
+        zIndex: 9
+      };
+    } 
+    else if (activeSlide - 2 === index) {
+      // Second card to the left (wider angle, further back)
+      return {
+        opacity: 1,
+        transform: "translateX(-480px) translateZ(-400px) rotateY(45deg)",
+        zIndex: 8
+      };
+    } 
+    else if (activeSlide + 2 === index) {
+      // Second card to the right (wider angle, further back)
+      return {
+        opacity: 1,
+        transform: "translateX(480px) translateZ(-400px) rotateY(-45deg)",
+        zIndex: 8
+      };
+    } 
+    else if (activeSlide - 3 === index) {
+      // Third card to the left (even wider angle, even further back)
+      return {
+        opacity: 1,
+        transform: "translateX(-720px) translateZ(-500px) rotateY(60deg)",
+        zIndex: 7
+      };
+    } 
+    else if (activeSlide + 3 === index) {
+      // Third card to the right (even wider angle, even further back)
+      return {
+        opacity: 1,
+        transform: "translateX(720px) translateZ(-500px) rotateY(-60deg)",
+        zIndex: 7
+      };
+    } 
+    else if (index < activeSlide - 3) {
+      // Cards further left (barely visible)
+      return {
+        opacity: 1,
+        transform: "translateX(-960px) translateZ(-600px) rotateY(75deg)",
+        zIndex: 6
+      };
+    } 
+    else if (index > activeSlide + 3) {
+      // Cards further right (barely visible)
+      return {
+        opacity: 1,
+        transform: "translateX(960px) translateZ(-600px) rotateY(-75deg)",
+        zIndex: 6
+      };
+    }
+  };
+  
+  
   return (
     <>
+      {/* carousel */}
       <div className="slideC">
-        {data.map((item, i) => (
+        {console.log('props.data:', props.data)}  
+        {props.data && props.data.map((item, i) => (  // Add null check
           <React.Fragment key={item.id}>
+            {console.log('Rendering item:', item)}
             <div
               className="slide"
-              style={getStyles(i)}
-              onClick={() => setActiveSlide(i)}
-              onDoubleClick={() => window.open(item.registrationForm, '_blank')}
+              style={{
+                ...getStyles(i)
+              }}
+              onClick={() => handleSlideClick(i)}
             >
               <SliderContent {...item} />
             </div>
-            <div className="reflection" style={getStyles(i)} />
+            <div
+              className="reflection"
+              style={{
+                ...getStyles(i)
+              }}
+            />
           </React.Fragment>
         ))}
       </div>
+      {/* carousel */}
     </>
   );
 };
 
-const SliderContent = ({ image }) => (
-  <div className="sliderContent">
-    <img
-      src={image}
+const SliderContent = (props) => {
+  return (
+    <div className="sliderContent">
+      <img src={`${props.image}`} 
       style={{
-        width: "100%",
-        objectFit: "cover",
+        width:"100%",
+        objectFit:"cover",
         borderRadius: "12px",
-      }}
-      alt="slide content"
-    />
-  </div>
-);
+      }}>
+      </img>
+    </div>
+  );
+};
 
-export default Slider;
